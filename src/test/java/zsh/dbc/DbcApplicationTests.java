4908cd506146ca3c9ac6325ec2a66f97f3100316
zsh <zsh.dev@outlook.com>
2019-09-13 14:01:14 +0800

    20190913-initial

---
+++ src/test/java/zsh/dbc/DbcApplicationTests.java
@@ -0,0 +1,16 @@
+package zsh.dbc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DbcApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
