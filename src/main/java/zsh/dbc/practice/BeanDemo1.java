215bb9708d7a169c63cda3598803a755318a3fa9
zsh <zsh.dev@outlook.com>
2019-10-18 14:03:39 +0800

    spring-core practice, 2019-10-18 14:03

---
+++ src/main/java/zsh/dbc/practice/BeanDemo1.java
@@ -0,0 +1,19 @@
+package zsh.dbc.practice;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import zsh.dbc.practice.bean.Console;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by zhengsihan, on 2019-10-18
+ */
+public class BeanDemo1 {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext("demo2.xml");
+        Console c = (Console) ac.getBean("console2");
+        c.hello();
+
+    }
+}
---
+++ src/main/java/zsh/dbc/practice/ReflPrac.java
@@ -0,0 +1,36 @@
+package zsh.dbc.practice;
+
+import java.lang.reflect.Method;
+
+/**
+ * Created by zhengsihan, on 2019-10-14
+ */
+public class ReflPrac {
+    //反射是为了解决在运行期，对某个实例一无所知的情况下，如何调用其方法
+    //没有import也能用
+
+    public static void main(String[] args) throws Exception {
+        // String对象:
+        String s = "Hello world";
+        // 获取String substring(int)方法，参数为int:
+        Method m = String.class.getMethod("substring", int.class);
+        // 在s对象上调用该方法并获取结果:
+        String r = (String) m.invoke(s, 6);
+        // 打印调用结果:
+        System.out.println(r);
+    }
+
+    static void printClassInfo(Class cls) {
+        System.out.println("Class name: " + cls.getName());
+        System.out.println("Simple name: " + cls.getSimpleName());
+        if (cls.getPackage() != null) {
+            System.out.println("Package name: " + cls.getPackage().getName());
+        }
+        System.out.println("is interface: " + cls.isInterface());
+        System.out.println("is enum: " + cls.isEnum());
+        System.out.println("is array: " + cls.isArray());
+        System.out.println("is primitive: " + cls.isPrimitive());
+    }
+
+
+}
