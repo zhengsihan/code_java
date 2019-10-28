3988e300b267c810d2775485eb04becb54564b45
zsh <zsh.dev@outlook.com>
2019-10-28 17:05:42 +0800

    https://www.bilibili.com/video/av35595465/?p=4

---
+++ prac2/src/main/java/org/minus/prac2/Prac2Application.java
@@ -0,0 +1,13 @@
+package org.minus.prac2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prac2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Prac2Application.class, args);
+    }
+
+}
---
+++ prac2/src/test/java/org/minus/prac2/Prac2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.minus.prac2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prac2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
