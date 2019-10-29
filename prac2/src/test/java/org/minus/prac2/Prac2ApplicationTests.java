d30dd04acb2c2f98102a3f29b4a1681bb2f3c41b
zsh <zsh.dev@outlook.com>
2019-10-29 13:51:17 +0800

    https://www.bilibili.com/watchlater/#/av35595465/p7 REDIS集成

--- prac2/src/test/java/org/minus/prac2/Prac2ApplicationTests.java
+++ prac2/src/test/java/org/minus/prac2/Prac2ApplicationTests.java
@@ -6,12 +6,19 @@ import org.minus.prac2.dto.ProductDto;
 import org.minus.prac2.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import javax.annotation.Resource;
 
 @SpringBootTest
 class Prac2ApplicationTests {
 
     @Autowired
     ProductService prodService;
+
+    @Autowired
+    @Resource
+    RedisTemplate redisTemplate;
 
     @Autowired
     ProductDao prodDao;
@@ -24,5 +31,12 @@ class Prac2ApplicationTests {
     public void testGetProdById(){
         ProductDto prodDto = prodService.getProdById(10004L);
         System.out.println(prodDto);
+    }
+
+    @Test
+    public void setValue() {
+        String key = "zsh";
+        Object value = redisTemplate.opsForValue().get(key);
+        System.out.println(String.format("%s的取值为：%s", key, value.toString()));
     }
 }
