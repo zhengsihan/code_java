42ba56bad78ebd945b70f2c7edd549c26235964e
zsh <zsh.dev@outlook.com>
2019-11-21 16:57:24 +0800

    精简

--- src/main/java/zsh/dbc/model/EditProdInfo.java
+++
@@ -1,7 +0,0 @@
-package zsh.dbc.model;
-
-/**
- * Created by zhengsihan, on 2019-10-18
- */
-public class EditProdInfo {
-}
--- src/main/java/zsh/dbc/practice/service/LogService.java
+++
@@ -1,8 +0,0 @@
-package zsh.dbc.practice.service;
-
-/**
- * Created by zhengsihan, on 2019-10-20
- */
-public interface LogService {
-    void sayHi();
-}
