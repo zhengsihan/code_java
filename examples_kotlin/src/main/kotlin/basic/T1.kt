68ead323df1edf5067ea42062618175908511165
zsh <zsh.dev@outlook.com>
2020-09-01 22:23:12 +0800

    20200901, v2

--- examples_kotlin/src/main/kotlin/basic/T1.kt
+++ examples_kotlin/src/main/kotlin/basic/T1.kt
@@ -1,7 +1,53 @@
 package basic
 
+class T1(val name: String){
+    init {
+        println("T1 object created!")
+    }
+    fun print() {
+        println("The name is $name")
+    }
+}
 fun main() {
-    f3()
+    f11()
+//    println(f8("zsh"))
+}
+
+fun f11() {
+    var t = T1("zsh")
+    t.print()
+    println(t.name)
+}
+
+fun f10() {
+    for(i in 5 downTo 1 step 2){
+        println(i)
+    }
+}
+
+fun f9() {
+    for(i in 1..3) {
+        println(i)
+    }
+}
+
+fun f8(str: String): String {
+    return "Param is $str"
+}
+
+fun f7(): String {
+    return "this is function seven!"
+}
+
+fun f6(): Int {
+    return 6;
+}
+
+fun f5() {
+    var myArr = arrayOf(1, 2, 3)
+    println(myArr[1])
+    println(myArr.size)
+    println("myArr have ${myArr.size} items")
 }
 
 fun f1() {
@@ -24,4 +70,10 @@ fun f3() {
     }else {
         println("b")
     }
+}
+
+fun f4() {
+    var a: Int = 3;
+    a = 128
+    println(a)
 }
